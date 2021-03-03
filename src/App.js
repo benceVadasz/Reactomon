b2f8daa34925df2ebbc51186098a59d7896e9b78
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-03-03 08:56:34 +0100

    JS files created that I will need

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Router>
+        <Navbar/>
+        <Route exact path="/pokemons/:id" children = {<PokemonDetail />} /> 
+        <Route exact path="/pokemons" children = {<Pokemons pokemons={pokemons}/>} />
+        <Route exact path="/types" children = {<Types exact path="/types" types={types}/>}/>
+        </Router> */}
     </div>
   );
 }
