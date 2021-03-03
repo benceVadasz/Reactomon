494c9223ab9e4705f3bba647e369ede82df096dc
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-03-03 08:58:24 +0100

    basic routing added to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Pokemons from './pages/Pokemons';
+import axios from 'axios'
+import Types from './Types';
+import PokemonDetail from './components/PokemonDetail';
 
 function App() {
   return (
     <div className="App">
-      {/* <Router>
+      <Router>
         <Navbar/>
         <Route exact path="/pokemons/:id" children = {<PokemonDetail />} /> 
         <Route exact path="/pokemons" children = {<Pokemons pokemons={pokemons}/>} />
         <Route exact path="/types" children = {<Types exact path="/types" types={types}/>}/>
-        </Router> */}
+        </Router>
     </div>
   );
 }
